9bf651bfb7edc57a67c65533b61efc6bebe269bf
Shoel Uddin <shoeluddin@Shoels-MacBook-Pro.local>
2020-12-14 09:59:43 -0500

    finished exercise

--- index.js
+++ index.js
@@ -18,17 +18,63 @@ app.set('view engine', 'html');
 
 app.use(logger)
 app.use(helmet())
+app.use(express.urlencoded ({extended: true}))
 
 // app.get ('/', (req,res)=> {
 //     res.send('Hello World')
 // })
 
-const arrObj =[
-    "Computer",
-    "Key-Board",
-    "Mouse",
-    "Glasses",
-    "Headphones"
+const arrEmploy =[
+    {
+        firstName: "Elsie",
+        lastName: "Orn",
+        jobTitle: "Chief Communications Officer",
+        jobArea: "Intranet",
+        prefix: "Miss",
+        suffix: "II",
+        birthDate: "2013-02-19T13:58:22.372Z",
+        startDate: "2015-12-20T17:25:49.141Z"
+    },
+    {
+        firstName: "Caroline",
+        lastName: "Trantow",
+        jobTitle: "International Directives Designer",
+        jobArea: "Integration",
+        prefix: "Mr.",
+        suffix: "Sr.",
+        birthDate: "2011-10-06T20:51:25.167Z",
+        startDate: "2012-12-03T17:18:37.858Z"
+    },
+    {
+        firstName: "Josh",
+        lastName: "Ritchie",
+        jobTitle: "Internal Brand Consultant",
+        jobArea: "Quality",
+        prefix: "Dr.",
+        suffix: "PhD",
+        birthDate: "1995-01-07T14:36:20.664Z",
+        startDate: "2011-02-09T15:15:46.525Z"
+    },
+    {
+        firstName: "Leopold",
+        lastName: "Hahn",
+        jobTitle: "Forward Marketing Coordinator",
+        jobArea: "Communications",
+        prefix: "Miss",
+        suffix: "I",
+        birthDate: "2007-06-07T18:42:04.474Z",
+        startDate: "2020-08-24T02:37:21.032Z"
+    },
+    {
+        firstName: "Yoshiko",
+        lastName: "Gaylord",
+        jobTitle: "Corporate Identity Associate",
+        jobArea: "Mobility",
+        prefix: "Dr.",
+        suffix: "II",
+        birthDate: "2004-10-23T18:40:12.004Z",
+        startDate: "2013-10-13T17:08:59.341Z"
+    }
 ]
 
 // Needed for templates rendering
@@ -40,10 +86,48 @@ app.get('/', (req,res)=>{
     })
 })
 
-app.get('/items', (req,res)=>{
-    res.json(arrObj)
+app.get('/list', (req,res)=>{
+    //res.send(`${employee}`)
+    //res.json(employee)
+    res.render('list', {
+        locals: {
+            arrEmploy
+        }
+    })
 })
 
+app.get('/list/:firstName', (req,res)=>{
+    const {firstName} = req.params
+    const employee = arrEmploy.find(e => e.firstName === firstName)
+    
+    res.render('details', {
+        locals : {
+            employee
+        }
+    })
+})
+
+app.get('/create', (req, res)=> {
+    res.render('create')
+})
+
+const db =[]
+
+app.post('/create', (req,res)=>{
+    const {Note} = req.body
+    db.push (Note)
+    //res.send('Thank You')
+    res.redirect('/thank-you')
+
+})
+
+app.get('/thank-you', (req,res)=> {
+    res.render('thank-you', {
+        locals: {
+            db
+        }
+    })
+})
 
 app.get('*', (req,res)=>{
     res.status(404).send('<h1>This is not the page your looking for!</h1>')
--- kata-1.md
+++ kata-1.md
@@ -21,42 +21,42 @@ If pairing:
 
 - [X] Create an array of objects (global variable) in your `index.js`
 
-- Add an `/items` route that sends the whole array using `res.json()`
+- [X] Add an `/list` route that sends the whole array using `res.json()`
 
-- Add a template for `/items`
+- [X] Add a template for `/list`
 
-- In the template, .map() the array in the template, showing 1 property in a `<li>`
+- [X] In the template, .map() the array in the template, showing 1 property in a `<li>`
 
-- Modify the .map(), showing a 2nd property
+- [X] Modify the .map(), showing a 2nd property
 
-- Add a route with a param for details (corresponding to one of the obj properties, like an id)
+- [X] Add a route with a param for details (corresponding to one of the obj properties, like an id)
 
-- `res.send()` back the route param (so that when you go to `/items/:id`, you see the id)
+- [X] `res.send()` back the route param (so that when you go to `/items/:id`, you see the id)
 
-- Use res.json() to send the obj from the array, specified by the param
+- [X] Use res.json() to send the obj from the array, specified by the param
 
-- Update the list template (that appears on `/list`) so that each `<li>` has `<a>` that goes to details route
+- [X] Update the list template (that appears on `/items`) so that each `<li>` has `<a>` that goes to details route
 
-- Add a template for the details page
+- [X] Add a template for the details page
 
-- Show the obj props in the template for the details page
+- [X] Show the obj props in the template for the details page
 
-- Add `app.use(express.urlencoded{extended: true})` so express can decode form submissions
+- [X] Add `app.use(express.urlencoded ({extended: true}))` so express can decode form submissions
 
-- Add a pair of routes (.get and .post) for the same path: `/create`
+- [X] Add a pair of routes (.get and .post) for the same path: `/create`
 
-- Create a template for the `/create` form, with a form element that starts with `<form method="post">`
+- [X] Create a template for the `/create` form, with a form element that starts with `<form method="post">`
 
-- `res.render()` the create form at the `app.get()` route
+- [X] `res.render()` the create form at the `app.get()` route
 
-- In `app.post()` route, use values from `req.body` to `.push()` the new value to global array
+- [X] In `app.post()` route, use values from `req.body` to `.push()` the new value to global array
 
-- `res.send()` the text "thank you" from the `.post()` route
+- [X] `res.send()` the text "thank you" from the `.post()` route
 
-- Add a `/thank-you` route
+- [X] Add a `/thank-you` route
 
-- Add a `/thank-you` template
+- [X] Add a `/thank-you` template
 
-- `res.redirect()` to /thank-you from the .post route
+- [X] `res.redirect()` to /thank-you from the .post route
 
 
